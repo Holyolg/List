74313ef08c0413c5e1cc9d89734003bd3b007659
Holyolg <oleg.chikolovec@gmail.com>
2024-06-17 14:07:06 +0300

    fix img root

--- src/components/app-info/app-info.tsx
+++ src/components/app-info/app-info.tsx
@@ -10,19 +10,19 @@ const AppInfo = ({
 	return (
 		<div className="app-info">
 			<div className="user-info">
-				<img className="user-icon" src='/img/icons/user.png' alt="employees" />
+				<img className="user-icon" src="img/icons/user.png" alt="employees" />
 				<h1>Учет сотрудников</h1>
-				</div>
-				<div className="user-info">
-				<img className="user-icon" src='/img/icons/users.png' alt="employees" />
+			</div>
+			<div className="user-info">
+				<img className="user-icon" src="img/icons/users.png" alt="employees" />
 
 				<h2>Общее число сотрудников: {employeesNum} </h2>
-				</div>
-				<div className="user-info">
+			</div>
+			<div className="user-info">
 				<div className="user-icon fas fa-cookie"></div>
 
 				<h2>Премию получат: {increased} </h2>
-				</div>
+			</div>
 		</div>
 	);
 };
